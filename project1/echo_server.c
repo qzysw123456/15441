5ed14532f48d11bd847d06da67fcec8286ed48b3
QZY <qzysw123456@gmail.com>
2018-06-12 00:51:39 -0500

    simple select based server

--- project1/echo_server.c
+++ project1/echo_server.c
@@ -1,21 +1,8 @@
-/******************************************************************************
-* echo_server.c                                                               *
-*                                                                             *
-* Description: This file contains the C source code for an echo server.  The  *
-*              server runs on a hard-coded port and simply write back anything*
-*              sent to it by connected clients.  It does not support          *
-*              concurrent clients.                                            *
-*                                                                             *
-* Authors: Athula Balachandran <abalacha@cs.cmu.edu>,                         *
-*          Wolf Richter <wolf@cs.cmu.edu>                                     *
-*                                                                             *
-*******************************************************************************/
-
 #include <netinet/in.h>
 #include <netinet/ip.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
@@ -24,94 +11,86 @@
 
 int close_socket(int sock)
 {
-    if (close(sock))
-    {
-        fprintf(stderr, "Failed closing socket.\n");
-        return 1;
-    }
-    return 0;
+	if (close(sock)) {
+		fprintf(stderr, "Failed closing socket.\n");
+		return 1;
+	}
+	return 0;
 }
+int open_socket()
+{
+	int sock;
+	struct sockaddr_in addr;
+	if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
+		fprintf(stderr, "Failed creating socket.\n");
+		return EXIT_FAILURE;
+	}
+	
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(ECHO_PORT);
+	addr.sin_addr.s_addr = INADDR_ANY;
 
+	if (bind(sock, (struct sockaddr *) &addr, sizeof(addr))) {
+		close_socket(sock);
+		fprintf(stderr, "Failed binding socket.\n");
+		return EXIT_FAILURE;
+	}
+	
+	if (listen(sock, 10)) {
+		close_socket(sock);
+		fprintf(stderr, "Error listening on socket.\n");
+		return EXIT_FAILURE;
+	}
+	
+	return sock;
+}
 int main(int argc, char* argv[])
 {
-    int sock, client_sock;
-    ssize_t readret;
-    socklen_t cli_size;
-    struct sockaddr_in addr, cli_addr;
-    char buf[BUF_SIZE];
+	int client_sock;
+	ssize_t readret;
+	socklen_t cli_size;
+	struct sockaddr_in cli_addr;
+	char buf[BUF_SIZE];
 
-    fprintf(stdout, "----- Echo Server -----\n");
-    
-    /* all networked programs must create a socket */
-    if ((sock = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        fprintf(stderr, "Failed creating socket.\n");
-        return EXIT_FAILURE;
-    }
+	fprintf(stdout, "----- Echo -----\n");
+	
+	int sock = open_socket();
+	
+	fd_set master;
+	fd_set read_fds;
+	int fdmax;
+	
+	fdmax = sock;
+	
+	FD_ZERO(&master);
+	FD_ZERO(&read_fds);
+	
+	FD_SET(sock, &master);	
+	
+	while("server forever")
+	{
+		read_fds = master;
+		if (select(fdmax+1, &read_fds, NULL, NULL, NULL) == -1)	{
+			fprintf(stderr, "fail to select");
+			exit(4);
+		}
+		for(int i = 0; i <= fdmax; i++) {
+			if(FD_ISSET(i, &read_fds)) {
+				if(i == sock) {
+					cli_size = sizeof cli_addr;
+					client_sock = accept(sock, (struct sockaddr* )&cli_addr, &cli_size);	
+					FD_SET(client_sock, &master);
+					if(client_sock > fdmax)
+						fdmax = client_sock;
+				}
+				else {
+					readret = recv(i, buf, sizeof buf, 0);
+					send(i, buf, readret, 0);
+				}
+			}
+		}	
+	}
+	close_socket(sock);
 
-    addr.sin_family = AF_INET;
-    addr.sin_port = htons(ECHO_PORT);
-    addr.sin_addr.s_addr = INADDR_ANY;
-
-    /* servers bind sockets to ports---notify the OS they accept connections */
-    if (bind(sock, (struct sockaddr *) &addr, sizeof(addr)))
-    {
-        close_socket(sock);
-        fprintf(stderr, "Failed binding socket.\n");
-        return EXIT_FAILURE;
-    }
-
-
-    if (listen(sock, 5))
-    {
-        close_socket(sock);
-        fprintf(stderr, "Error listening on socket.\n");
-        return EXIT_FAILURE;
-    }
-
-    /* finally, loop waiting for input and then write it back */
-    while (1)
-    {
-       cli_size = sizeof(cli_addr);
-       if ((client_sock = accept(sock, (struct sockaddr *) &cli_addr,
-                                 &cli_size)) == -1)
-       {
-           close(sock);
-           fprintf(stderr, "Error accepting connection.\n");
-           return EXIT_FAILURE;
-       }
-       
-       readret = 0;
-
-       while((readret = recv(client_sock, buf, BUF_SIZE, 0)) >= 1)
-       {
-           if (send(client_sock, buf, readret, 0) != readret)
-           {
-               close_socket(client_sock);
-               close_socket(sock);
-               fprintf(stderr, "Error sending to client.\n");
-               return EXIT_FAILURE;
-           }
-           memset(buf, 0, BUF_SIZE);
-       } 
-
-       if (readret == -1)
-       {
-           close_socket(client_sock);
-           close_socket(sock);
-           fprintf(stderr, "Error reading from client socket.\n");
-           return EXIT_FAILURE;
-       }
-
-       if (close_socket(client_sock))
-       {
-           close_socket(sock);
-           fprintf(stderr, "Error closing client socket.\n");
-           return EXIT_FAILURE;
-       }
-    }
-
-    close_socket(sock);
-
-    return EXIT_SUCCESS;
+	return 0;
 }
